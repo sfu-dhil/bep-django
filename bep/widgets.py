7445724462dbf94f3c23a704f049f9d52f61d1c8
Andrew Gardener <andrew.e.gardener@gmail.com>
2025-06-23 14:26:38 -0700

    Fix admin new item when using select2array widget

--- bep/widgets.py
+++ bep/widgets.py
@@ -4,18 +4,14 @@ class Select2TagArrayWidget(Select2AdminMixin, Select2TagWidget):
     def build_attrs(self, base_attrs, extra_attrs=None):
         default_attrs = {
             "data-allow-clear": False,
-            "data-token-separators": '|',
+            "data-token-separators": ',',
             "data-width": '500px',
         }
         default_attrs.update(base_attrs)
         return super().build_attrs(default_attrs, extra_attrs=extra_attrs)
 
-    def value_from_datadict(self, data, files, name):
-        values = super().value_from_datadict(data, files, name)
-        return "|".join(values)
-
     def optgroups(self, name, value, attrs=None):
-        values = value[0].split('|') if value[0] else []
+        values = value[0].split(',') if len(value) > 0 and value[0] else []
         selected = set(values)
         subgroup = [self.create_option(name, v, v, selected, i) for i, v in enumerate(values)]
         return [(None, subgroup, 0)]
